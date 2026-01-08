4778cd129ffab4626383e6c50ff2a7b5da4928c3
Showtime-Mitja <mitja2.stefanec@gmail.com>
2026-01-08 16:40:16 +0100

    -Enabled and added XML documentation -Added XML comments for the API -Added Title and Version for the API via SwaggerDoc

--- ProductApi/Controllers/ProductsController.cs
+++ ProductApi/Controllers/ProductsController.cs
@@ -15,12 +15,26 @@ namespace ProductApi.Controllers
             _repo = repo; 
         }
 
+        /// <summary> 
+        /// Returns products filtered by name, category or price range. 
+        /// </summary> 
+        /// <param name="name">Name filter</param> 
+        /// <param name="categoryId">Category ID filter</param> 
+        /// <param name="minPrice">Minimum price</param> 
+        /// <param name="maxPrice">Maximum price</param> 
+        /// <returns>A list of products matching the filter criteria</returns>
         [HttpGet]
         public IActionResult GetFiltered([FromQuery] string? name, [FromQuery] int? categoryId, [FromQuery] decimal? minPrice, [FromQuery] decimal? maxPrice)
         {
             return Ok(_repo.GetFiltered(name, categoryId, minPrice, maxPrice));
         }
 
+        /// <summary> 
+        /// Updates an existing products name and/or price. 
+        /// </summary> 
+        /// <param name="id">ID of the product to update</param> 
+        /// <param name="dto">The updated product values</param> 
+        /// <returns>No content if product has been successfully updated or an error response</returns>
         [HttpPut("{id}")]
         public IActionResult Update(int id, [FromBody] UpdateProductDto dto)
         {
--- ProductApi/Program.cs
+++ ProductApi/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.OpenApi;
 using ProductApi.Repositories;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,8 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddEndpointsApiExplorer(); 
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API for retrieving and updating products",
+        Version = "v1"
+    });
+});
+
 builder.Services.AddSingleton<IProductRepository, InMemoryProductRepository>();
 
 var app = builder.Build();
